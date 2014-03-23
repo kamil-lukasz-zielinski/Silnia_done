4bb84e39731edb0a81ce094307b1aef10c14df1b
kamil-lukasz-zielinski <zielinski@live.jp>
2014-03-23 18:47:54 +0100

    Update KalkulatorSilni.java

--- src/main/java/javahive/silnia/KalkulatorSilni.java
+++ src/main/java/javahive/silnia/KalkulatorSilni.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package javahive.silnia;
-/**
- * nowa uwaga - test gita
- * @author Marcin Grabowiecki
- */
 
 public interface KalkulatorSilni {
-    public String licz(int arg) throws ArithmeticException;
+    public String licz(int arg);
 }
