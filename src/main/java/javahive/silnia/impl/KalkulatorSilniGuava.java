6b171c0b0c9346f94f257e33888ac7721f50c92c
kamil-lukasz-zielinski <zielinski@live.jp>
2014-03-23 18:50:19 +0100

    Update KalkulatorSilniGuava.java

--- src/main/java/javahive/silnia/impl/KalkulatorSilniGuava.java
+++ src/main/java/javahive/silnia/impl/KalkulatorSilniGuava.java
@@ -1,13 +1,7 @@
 package javahive.silnia.impl;
-
 import com.google.common.math.BigIntegerMath;
-
 import javahive.silnia.KalkulatorSilni;
 
-/**
- *
- * @author mgr
- */
 public class KalkulatorSilniGuava implements KalkulatorSilni{
 
     public String licz(int arg) {
