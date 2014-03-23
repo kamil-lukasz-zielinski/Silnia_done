582f23d44f95aeff067c9e95264fdaea3f6ab6e2
kamil-lukasz-zielinski <zielinski@live.jp>
2014-03-23 19:05:48 +0100

    Update SzybkiKalkulatorSilni.java

--- src/main/java/javahive/silnia/impl/SzybkiKalkulatorSilni.java
+++ src/main/java/javahive/silnia/impl/SzybkiKalkulatorSilni.java
@@ -3,12 +3,13 @@ import javahive.silnia.KalkulatorSilni;
 
 public class SzybkiKalkulatorSilni implements KalkulatorSilni{
     public long liczLong(long podstawa){
-    	if(podstawa<0) {
-    		throw new ArithmeticException();
-    	}
+        if(podstawa<0) {
+            throw new ArithmeticException();
+        }
         if(podstawa>0) {
             return podstawa*liczLong(podstawa-1);
-        } else {
+        } 
+        else {
             return 1;
         }
     }
