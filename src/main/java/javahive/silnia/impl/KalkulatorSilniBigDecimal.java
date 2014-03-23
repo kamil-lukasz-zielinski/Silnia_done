5cb8f99b1fdea443e7e4efb08499cde62ad79132
kamil-lukasz-zielinski <zielinski@live.jp>
2014-03-23 19:01:46 +0100

    Update KalkulatorSilniBigDecimal.java

--- src/main/java/javahive/silnia/impl/KalkulatorSilniBigDecimal.java
+++ src/main/java/javahive/silnia/impl/KalkulatorSilniBigDecimal.java
@@ -1,23 +1,22 @@
 package javahive.silnia.impl;
-
 import java.math.BigDecimal;
 import javahive.silnia.KalkulatorSilni;
 
 public class KalkulatorSilniBigDecimal implements KalkulatorSilni {
     public BigDecimal liczBigDecimal(BigDecimal podstawa) {
-    	if(podstawa.compareTo(BigDecimal.ZERO) == -1) {
-            throw new ArithmeticException();
-        }
-        if(podstawa.compareTo(BigDecimal.ZERO) == 1) {
-            return podstawa.multiply(liczBigDecimal(podstawa.subtract(BigDecimal.ONE)));
-        } 
-        else {
-            return BigDecimal.ONE;
-        }
+	    if(podstawa.compareTo(BigDecimal.ZERO) == -1) {
+	        throw new ArithmeticException();
+	    }
+	    if(podstawa.compareTo(BigDecimal.ZERO) == 1) {
+	        return podstawa.multiply(liczBigDecimal(podstawa.subtract(BigDecimal.ONE)));
+	    } 
+	    else {
+	        return BigDecimal.ONE;
+	    }
     }
     
     public String licz(int arg) {
-        BigDecimal score = new BigDecimal(String.valueOf(arg));
-        return ""+liczBigDecimal(score);
+	    BigDecimal score = new BigDecimal(String.valueOf(arg));
+	    return ""+liczBigDecimal(score);
     }
 }
