07849ab266c011ab220e90cf26022e3f068235eb
kamil-lukasz-zielinski <zielinski@live.jp>
2014-03-23 18:51:12 +0100

    Update SzybkiKalkulatorSilni.java

--- src/main/java/javahive/silnia/impl/SzybkiKalkulatorSilni.java
+++ src/main/java/javahive/silnia/impl/SzybkiKalkulatorSilni.java
@@ -1,21 +1,7 @@
 package javahive.silnia.impl;
-
 import javahive.silnia.KalkulatorSilni;
 
-
-/**
- *
- * @author Marcin Grabowiecki
- * 
- * Klasa licząca silnię, która wykorzystuje typy long.
- * nie jest to optymalne, ponieważ maksymalna wartość wynosi 3409530940953,
- * więc nie jesteśmy w stanie liczyć zbyt wielkich silni
- * używamy algorytmu rekurencyjnego
- */
-
-
 public class SzybkiKalkulatorSilni implements KalkulatorSilni{
-    //metoda licząca silnie w sposób rekurencyjny
     public long liczLong(long podstawa){
     	if(podstawa<0) {
     		throw new ArithmeticException();
